1a66e027e95ccc0ab2f621b038747594954fd65b
Douglas-Dias-494 <douglas.diasesilva@gmail.com>
2025-09-10 14:36:44 -0300

    Inicializando o projeto do Challenge Softtek FIAP

---
+++ src/main/java/br/com/fiap/backend_Softtek/BackendSofttekApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.backend_Softtek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSofttekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSofttekApplication.class, args);
+	}
+
+}
